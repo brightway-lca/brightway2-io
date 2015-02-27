1743fe801fa41c26cf6371738bdd63e29248a363
Chris Mutel <cmutel@gmail.com>
2015-02-27 14:09:28 +0100

    Add more SimaPro biosphere categories

--- bw2io/compatibility.py
+++ bw2io/compatibility.py
@@ -1,10 +1,14 @@
 SIMAPRO_BIO_SUBCATEGORIES = {
     u"groundwater": u'ground-',
     u"groundwater, long-term": u'ground-, long-term',
-    u"high. pop.": u'high population density',
-    u"low. pop.": u'low population density',
+    u"high. pop.": u'urban air close to ground',
+    u"low. pop.": u'non-urban air or from high stacks',
     u"low. pop., long-term": u'low population density, long-term',
     u"stratosphere + troposphere": u'lower stratosphere + upper troposphere',
+    u'river': u'surface water',
+    u'river, long-term': u'surface water',
+    u'lake': u'surface water',
+    u'(unspecified)': u'unspecified',
 }
 
 SIMAPRO_BIOSPHERE = {
@@ -13,7 +17,7 @@ SIMAPRO_BIOSPHERE = {
     u"Emissions to soil": u"soil",
     u"Emissions to water": u"water",
     u"Non material emissions": u"non-material",
-    u"Resources": u"resource",
+    u"Resources": u"natural resource",
     u"Social issues": u"social",
 }
 
@@ -21,4 +25,16 @@ SIMAPRO_SYSTEM_MODELS = {
     "apos": "Allocation, ecoinvent default",
     "consequential": "Substitution, consequential, long-term",
     "cutoff": "Allocation, cut-off by classification",
+}
+
+ECOSPOLD_2_3_BIOSPHERE = {
+   ('resource', 'in ground'): (u'natural resource', u'in ground'),
+   ('resource', 'in air'): (u'natural resource', u'in air'),
+   ('resource', 'in water'): (u'natural resource', u'in water'),
+   ('resource', 'land'): (u'natural resource', u'land'),
+   ('resource', 'biotic'): (u'natural resource', u'biotic'),
+   ('air', 'high population density'): (u'air', u'urban air close to ground'),
+   ('air', 'low population density'): (u'air', u'non-urban air or from high stacks'),
+   ('air', 'low population density, long-term'): (u'air', u'low population density, long-term'),
+   ('air', 'land'): (u'air', u'land'),
 }
