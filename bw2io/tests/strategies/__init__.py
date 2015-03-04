e75043ae1a4f4aae637be15479c8391f3015ed9c
Chris Mutel <cmutel@gmail.com>
2015-03-04 17:48:20 +0100

    Test for adding activity hash code

--- bw2io/tests/strategies/__init__.py
+++ bw2io/tests/strategies/__init__.py
@@ -8,3 +8,4 @@ from .biosphere import (
 from .simapro_name_splitting import NameSplittingTestCase
 from .simapro_normalization import SPNormalizationTestCase
 from .link_iterable import LinkIterableTestCase
+from .lcia import LCIATestCase
---
+++ bw2io/tests/strategies/lcia.py
@@ -0,0 +1,26 @@
+import unittest
+from ...strategies import add_activity_hash_code
+
+class LCIATestCase(unittest.TestCase):
+    def test_add_activity_hash_code(self):
+        data = [{
+            'exchanges': [{
+                'name': 'foo',
+                'code': 'bar'
+            }, {
+                'name': 'foo',
+            }]
+        }]
+        expected = [{
+            'exchanges': [{
+                'name': 'foo',
+                'code': 'bar'
+            }, {
+                'name': 'foo',
+                'code': 'acbd18db4cc2f85cedef654fccc4a4d8',
+            }]
+        }]
+        self.assertEqual(
+            expected,
+            add_activity_hash_code(data)
+        )
