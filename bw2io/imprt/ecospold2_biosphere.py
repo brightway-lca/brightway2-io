ae2d23bad3aedd4aafc44b6e85d1cc3421c80875
Chris Mutel <cmutel@gmail.com>
2015-03-04 09:43:09 +0100

    Actually add biosphere importer

---
+++ bw2io/imprt/ecospold2_biosphere.py
@@ -0,0 +1,47 @@
+from ..units import normalize_units
+from .base import ImportBase
+from ..strategies import drop_unspecified_subcategories
+from bw2data.utils import recursive_str_to_unicode
+from lxml import objectify
+import os
+
+EMISSIONS_CATEGORIES = {
+    "air":   "emission",
+    "soil":  "emission",
+    "water": "emission",
+}
+
+
+class Ecospold2BiosphereImporter(ImportBase):
+    strategies = [drop_unspecified_subcategories]
+
+    def __init__(self):
+        self.data = self.extract()
+        self.db_name = u'biosphere3'
+
+    def extract(self):
+        def extract_flow_data(o):
+            ds = {
+                'categories': (
+                    o.compartment.compartment.text,
+                    o.compartment.subcompartment.text
+                ),
+                'code': o.get('id'),
+                'name': o.name.text,
+                'database': 'biosphere3',
+                'exchanges': [],
+                'unit': normalize_units(o.unitName.text),
+            }
+            ds[u"type"] = EMISSIONS_CATEGORIES.get(
+                ds['categories'][0], ds['categories'][0]
+            )
+            return ds
+
+        fp = os.path.join(
+            os.path.dirname(__file__),
+            "..", "data", "lci",
+            "ecoinvent elementary flows 3.1.xml"
+        )
+        root = objectify.parse(open(fp)).getroot()
+        return recursive_str_to_unicode([extract_flow_data(ds)
+                                         for ds in root.iterchildren()])
