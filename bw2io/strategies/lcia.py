11e5d5a5c70b06adcf1d023f7828e5cea3bf95ec
Chris Mutel <cmutel@gmail.com>
2015-03-05 08:59:44 +0100

    Rewrite and test match_subcategories

--- bw2io/strategies/lcia.py
+++ bw2io/strategies/lcia.py
@@ -1,6 +1,7 @@
 from bw2data import mapping, Database
 from ..utils import activity_hash
 import collections
+import copy
 
 
 def add_activity_hash_code(data):
@@ -32,44 +33,38 @@ def set_biosphere_type(data):
 
 
 def match_subcategories(data, biosphere_db_name):
-    """For a set of top-level (i.e. only one category deep) CFs, try to match CFs to all existing subcategories.
+    """For a set of top-level (i.e. only one category deep) CFs, add CFs for all existing subcategories."""
+    def add_subcategories(obj):
+        # Sorting needed for tests
+        new_objs = sorted(mapping[(
+            obj['categories'][0],
+            obj['name'],
+            obj['unit'],
+        )])
+        return [{
+            'input': elem,
+            'amount': obj['amount']
+        } for elem in new_objs]
 
-    First, create a dict of biosphere hashes to categories. The hashes are computed using only the top-level category:
-
-        flow_category_mapping = {"some-hash": [("cat 1", ('bio db', 'bio code'))]}
-
-    For each method, skip the method if it has CFs for flows with subcategories. Otherwise, for each flow:
-
-        * Skip the flow if it already has a code
-        * Otherwise, rewrite the CF to match each existing (sub)category in the biosphere database.
-
-    """
-    def strip_subcategory(ds):
-        if 'categories' in ds:
-            ds['categories'] = ds['categories'][:1]
-        return ds
-
-    def rewrite_cf(cf, categories, key):
-        cf.update(code=key[1], categories=categories)
-        return cf
-
-    flow_category_mapping = collections.defaultdict(list)
+    mapping = collections.defaultdict(list)
     for flow in Database(biosphere_db_name):
-        flow_category_mapping[activity_hash(strip_subcategory(flow))
-                              ].append((flow['categories'], flow.key))
+        if not flow.get("type") == 'emission':
+            continue
+        if len(flow.get('categories', [])) > 1:
+            mapping[(
+                flow['categories'][0],
+                flow['name'],
+                flow['unit']
+            )].append(flow.key)
 
-    only_top_level_categories = lambda x: all([len(y['categories']) == 1
+    only_top_level_categories = lambda x: all([len(y.get('categories', [])) == 1
                                                for y in x])
 
     for method in data:
         if not only_top_level_categories(method['exchanges']):
             continue
-        cfs = [cf for cf in method['exchanges'] if cf.get('code')] + \
-              [cf for cf in method['exchanges']
-               if activity_hash(cf) not in flow_category_mapping] + \
-              [rewrite_cf(cf, categories, key)
-               for cf in method['exchanges']
-               for categories, key in flow_category_mapping.get(activity_hash(cf), [])
-               if not cf.get('code')]
-        method[u'exchanges'] = cfs
+        new_cfs = []
+        for obj in method['exchanges']:
+            new_cfs.extend(add_subcategories(obj))
+        method[u'exchanges'].extend(new_cfs)
     return data
--- bw2io/tests/strategies/lcia.py
+++ bw2io/tests/strategies/lcia.py
@@ -1,8 +1,10 @@
-from ...strategies import add_activity_hash_code
+from ...strategies import add_activity_hash_code, match_subcategories
+from bw2data import Database
+from bw2data.tests import BW2DataTest
 import unittest
 
 
-class LCIATestCase(unittest.TestCase):
+class LCIATestCase(BW2DataTest):
     def test_add_activity_hash_code(self):
         data = [{
             'exchanges': [{
@@ -24,4 +26,118 @@ class LCIATestCase(unittest.TestCase):
         self.assertEqual(
             expected,
             add_activity_hash_code(data)
+        )
+
+    def test_match_subcategories(self):
+        self.maxDiff = None
+        background = [
+            {
+                u'categories': (u'air', u'non-urban air or from high stacks'),
+                u'code': u'first',
+                u'database': u'b',
+                u'exchanges': [],
+                u'name': u'Boron trifluoride',
+                u'type': u'emission',
+                u'unit': u'kilogram'
+            }, {
+                u'categories': (u'air', u'low population density, long-term'),
+                u'code': u'second',
+                u'database': u'b',
+                u'exchanges': [],
+                u'name': u'Boron trifluoride',
+                u'type': u'emission',
+                u'unit': u'kilogram'
+            }, {
+                u'categories': (u'air', u'lower stratosphere + upper troposphere'),
+                u'code': u'third',
+                u'database': u'b',
+                u'exchanges': [],
+                u'name': u'Boron trifluoride',
+                u'type': u'emission',
+                u'unit': u'kilogram'
+            }, {  # Skip - root category
+                u'categories': (u'air',),
+                u'code': u'fourth',
+                u'database': u'b',
+                u'exchanges': [],
+                u'name': u'Boron trifluoride',
+                u'type': u'emission',
+                u'unit': u'kilogram'
+            }, {  # Should be skipped - wrong type
+                u'categories': (u'air', u'skip me'),
+                u'code': u'Bill. My friends just call me Bill.',
+                u'database': u'b',
+                u'exchanges': [],
+                u'name': u'Boron trifluoride',
+                u'type': u'something else',
+                u'unit': u'kilogram'
+            }
+        ]
+        db = Database('b')
+        db.register()
+        db.write({(obj['database'], obj['code']): obj
+                  for obj in background})
+
+        data = [{
+            'name': 'Some LCIA method',
+            'exchanges': [{
+                'name': 'Boron trifluoride',
+                'categories': ('air',),
+                'unit': 'kilogram',
+                # Only for CFs - no need for biosphere filter
+                # 'type': 'biosphere',
+                'amount': 1,
+            }]
+        }, {  # Not just root categories - skip
+            'name': 'Some other LCIA method',
+            'exchanges': [{
+                'name': 'Boron trifluoride',
+                'categories': ('air',),
+                'unit': 'kilogram',
+                'type': 'biosphere',
+                'amount': 1,
+            }, {
+                'name': 'Boron trifluoride',
+                'categories': ('air', 'another'),
+                'unit': 'kilogram',
+                'type': 'biosphere',
+                'amount': 1,
+            }]
+        }]
+        expected = [{
+            'name': 'Some LCIA method',
+            'exchanges': [{
+                'name': 'Boron trifluoride',
+                'categories': ('air',),
+                'unit': 'kilogram',
+                'amount': 1,
+            }, {
+                'input': ('b', 'first'),
+                'amount': 1,
+            }, {
+                'input': ('b', 'second'),
+                'amount': 1,
+            }, {
+                'input': ('b', 'third'),
+                'amount': 1,
+            }]
+        }, {
+            'name': 'Some other LCIA method',
+            'exchanges': [{
+                'name': 'Boron trifluoride',
+                'categories': ('air',),
+                'unit': 'kilogram',
+                'type': 'biosphere',
+                'amount': 1,
+            }, {
+                'name': 'Boron trifluoride',
+                'categories': ('air', 'another'),
+                'unit': 'kilogram',
+                'type': 'biosphere',
+                'amount': 1,
+            }]
+        }]
+        self.assertEqual(
+            expected,
+            match_subcategories(data, 'b')
         )
