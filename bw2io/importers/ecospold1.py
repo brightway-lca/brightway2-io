996f7b56e98da874fba326adcb5e46ea3d1d0ff5
Chris Mutel <cmutel@gmail.com>
2015-03-05 09:49:48 +0100

    Add docs and rationalize ecospold1 importer

--- bw2io/importers/ecospold1.py
+++ bw2io/importers/ecospold1.py
@@ -6,8 +6,7 @@ from ..strategies import (
     clean_integer_codes,
     drop_unspecified_subcategories,
     es1_allocate_multioutput,
-    link_external_technosphere_by_activity_hash,
-    link_internal_technosphere_by_activity_hash,
+    link_technosphere_by_activity_hash,
     link_iterable_by_fields,
     normalize_biosphere_categories,
     normalize_biosphere_names,
@@ -20,21 +19,27 @@ import functools
 
 
 class SingleOutputEcospold1Importer(LCIImporter):
-    """The default strategy will already set the single product as reference product, name, etc.
+    """Import and process single-output datasets in the ecospold 1 format.
 
     Applies the following strategies:
     #. If only one exchange is a production exchange, that is the reference product
+    #. Delete (unreliable) integer codes from extracted data
     #. Drop ``unspecified`` subcategories from biosphere flows
     #. Normalize biosphere flow categories to ecoinvent 3.1 standard
     #. Normalize biosphere flow names to ecoinvent 3.1 standard
+    #. Remove locations from biosphere exchanges
     #. Create a ``code`` from the activity hash of the dataset
-    #. Link to the default biosphere database by name, unit, categories
-    #. Link internal technosphere exchanges by name, unit, location, categories
-    #. Mark unlinked exchanges
+    #. Link biosphere exchanges to the default biosphere database
+    #. Link internal technosphere exchanges
+
+    Args:
+        * *filepath*: Either a file or directory.
+        * *db_name*: Name of database to create.
 
     """
     strategies = [
         assign_only_product_as_production,
+        clean_integer_codes,
         drop_unspecified_subcategories,
         normalize_biosphere_categories,
         normalize_biosphere_names,
@@ -44,7 +49,7 @@ class SingleOutputEcospold1Importer(LCIImporter):
             other=Database(config.biosphere),
             kind='biosphere'
         ),
-        link_internal_technosphere_by_activity_hash,
+        link_technosphere_by_activity_hash,
     ]
     format = u"Ecospold1"
 
@@ -55,29 +60,33 @@ class SingleOutputEcospold1Importer(LCIImporter):
         print(u"Extracted {} datasets in {:.2f} seconds".format(
               len(self.data), time() - start))
 
-    def match_background(self, background_db):
-        """Link exchanges to background database named ``background_db`` using activity hashes"""
-        func_list = [
-            functools.partial(
-                link_external_technosphere_by_activity_hash,
-                external_db_name=background_db
-            )
-        ]
-        self.apply_strategies(func_list)
-
 
 class MultiOutputEcospold1Importer(SingleOutputEcospold1Importer):
+    """Import and process mutli-output datasets in the ecospold 1 format.
+
+    Applies the following strategies:
+    #. Allocate multioutput datasets to single-output datasets
+    #. Drop ``unspecified`` subcategories from biosphere flows
+    #. Drop (unreliable) integer codes from extracted data
+    #. If only one exchange is a production exchange, that is the reference product
+    #. Normalize biosphere flow categories to ecoinvent 3.1 standard
+    #. Normalize biosphere flow names to ecoinvent 3.1 standard
+    #. Remove locations from biosphere exchanges
+    #. Create a ``code`` from the activity hash of the dataset
+    #. Link biosphere exchanges to the default biosphere database
+    #. Link internal technosphere exchanges"""
     strategies = [
         es1_allocate_multioutput,
         drop_unspecified_subcategories,
         clean_integer_codes,
         assign_only_product_as_production,
+        normalize_biosphere_categories,
+        normalize_biosphere_names,
         strip_biosphere_exc_locations,
         set_code_by_activity_hash,
         functools.partial(link_iterable_by_fields,
             other=Database(config.biosphere),
             kind='biosphere'
         ),
-        link_internal_technosphere_by_activity_hash,
+        link_technosphere_by_activity_hash,
     ]
-
