024e6900cae26f32f379c6e3386c1191a9b1edd1
Chris Mutel <cmutel@gmail.com>
2015-11-02 23:51:49 +0100

    0.2.dev5

--- bw2io/__init__.py
+++ bw2io/__init__.py
@@ -26,7 +26,7 @@ __all__ = [
     'UnlinkedData',
 ]
 
-__version__ = (0, 2, "dev4")
+__version__ = (0, 2, "dev5")
 
 
 from .package import BW2Package
--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import os
 
 setup(
     name='bw2io',
-    version="0.2.dev4",
+    version="0.2.dev5",
     packages=[
         'bw2io',
         'bw2io.data',
