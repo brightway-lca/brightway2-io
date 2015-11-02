bcadbc8dfd765f0ee1dab6b83127a45db6e76fc7
Chris Mutel <cmutel@gmail.com>
2015-11-02 23:51:22 +0100

    Exiobase; more generators, more docs

--- bw2io/importers/exiobase.py
+++ bw2io/importers/exiobase.py
@@ -6,10 +6,6 @@ from bw2data.backends.iotable import IOTableBackend
 from bw2data import Database
 from ..extractors import ExiobaseDataExtractor
 from ..strategies import (
-    # normalize_biosphere_categories,
-    # normalize_biosphere_names,
-    # normalize_units,
-    # link_iterable_by_fields,
     migrate_datasets,
 )
 from ..utils import activity_hash
@@ -26,6 +22,7 @@ class Exiobase22Importer(object):
     def __init__(self, filepath, db_name="EXIOBASE 2.2"):
         self.strategies = []
         self.db_name = db_name
+        print("Creating database {}".format(self.db_name))
         start = time()
         self.outputs = ExiobaseDataExtractor.extract(filepath)
 
@@ -34,38 +31,15 @@ class Exiobase22Importer(object):
             time() - start
         ))
 
-    def process_raw_data(self, biosphere="biosphere3"):
-        print("Aggregating `substances` and `extractions`")
-        self.biosphere = [{
-            'name': obj[0],
-            'exiobase-code': obj[1],
-            'synonym': obj[2],
-            'description': obj[3] or None
-        } for obj in self.outputs['substances']] + [{
-            'name': obj[1],
-            'synonym': obj[2]
-        } for obj in self.outputs['extractions']]
+    def apply_strategies(self, biosphere=None):
+        # Plus normalize units
 
-        for obj in self.biosphere:
-            obj['old-name'] = obj['name'][:]
-
-        self.biosphere = migrate_datasets(self.biosphere, "exiobase-biosphere")
-
-        biosphere_hashes = {activity_hash(obj, fields=["name", "categories"]): obj.key for obj in Database(biosphere)}
-        self.biosphere = {
-            obj['old-name']: biosphere_hashes[activity_hash(obj, fields=["name", "categories"])]
-            for obj in self.biosphere
-        }
-
-    def apply_strategies(self):
-        print("Processing biosphere")
-        self.biosphere = extract_exiobase_biosphere(
-            self.outputs['emissions'] + self.outputs['resources'],
+        self.biosphere_lookup = get_biosphere_lookup_dict(
             self.outputs['substances'],
-            self.db_name
+            self.outputs['extractions'],
+            biosphere
         )
 
-        # Plus normalize units
         print("Processing technosphere")
         self.activities = extract_exiobase_technosphere(
             self.outputs['industries'],
@@ -73,7 +47,6 @@ class Exiobase22Importer(object):
             self.db_name
         )
 
-        # Plus normalize units
         print("Processing exchanges")
         self.exchanges = itertools.chain(
             relabel_exchanges(
@@ -82,35 +55,43 @@ class Exiobase22Importer(object):
             ),
             relabel_emissions(
                 self.outputs['emissions'],
-                self.db_name
+                self.db_name,
+                self.biosphere_lookup
             )
         )
 
-    def write_databases(self):
-        bd = Database("{} biosphere".format(self.db_name))
-        bd.write(self.biosphere)
+    def write_database(self):
         mrio = IOTableBackend(self.db_name)
         mrio.write(self.activities, self.exchanges)
 
 
-def extract_exiobase_biosphere(emissions_table, substances_table, db_name):
-    # Get list of all possible biosphere exchanges
-    biosphere_set = {(line[2], line[3], line[4]) for line in emissions_table}
-    substance_dict = {obj[0]: obj for obj in substances_table}
-    biosphere = [{
+def get_biosphere_lookup_dict(substances, extractions, biosphere=None):
+    print("Aggregating `substances` and `extractions`")
+    lookup = [{
         'name': obj[0],
-        'code': obj[0],
-        'exiobase_code': substance_dict[obj[0]][1],
-        'database': "{} biosphere".format(db_name),
-        'categories': [obj[1]],
-        'synonym': substance_dict[obj[0]][2],
-        'unit': obj[2].replace("/M.EUR", ""),
-        'type': 'emission',
-    } for obj in biosphere_set]
-    return {(obj['database'], obj['code']): obj for obj in biosphere}
+        'exiobase-code': obj[1],
+        'synonym': obj[2],
+        'description': obj[3] or None
+    } for obj in substances] + [{
+        'name': obj[1],
+        'synonym': obj[2]
+    } for obj in extractions]
+
+    for obj in lookup:
+        obj['old-name'] = obj['name'][:]
+
+    lookup = migrate_datasets(lookup, "exiobase-biosphere")
+
+    biosphere_hashes = {activity_hash(obj, fields=["name", "categories"]): obj.key
+                        for obj in Database(biosphere or "biosphere3")}
+    return {
+        obj['old-name']: biosphere_hashes[activity_hash(obj, fields=["name", "categories"])]
+        for obj in lookup
+    }
 
 
 def extract_exiobase_technosphere(industries, countries, db_name):
+    """Create activity datasets for each combination of `industries` and `countries`."""
     data = [{
         'name': obj[1],
         'code': obj[1],
@@ -127,16 +108,40 @@ def extract_exiobase_technosphere(industries, countries, db_name):
     return {obj['key']: obj for obj in data}
 
 
-def relabel_emissions(emissions_table, db_name):
-    data = [
+def relabel_emissions(emissions_table, db_name, lookup):
+    """Turn rows into a generator of (flow, process, type, amount) tuples.
+
+    Original data format:
+
         (
-            ("{} biosphere".format(db_name), row[2]),
+            'AT',
+            'Cultivation of wheat',
+            'CO2 - combustion',
+            'air',
+            'kg/M.EUR',
+            289687.6972210754
+        )
+
+    * `emissions_table` is the list of raw data lines.
+    * `db_name` is the string name of the database, 'Exiobase 2.2' by default.
+    * `lookup` is a dictionary from string flow names to biosphere keys.
+
+    Returns:
+
+        (
+            ("biosphere3", "some-code"),  # Looks up 'CO2 - combustion' in `lookup`
+            ("Exiobase 2.2", "Cultivation of wheat:AT"),
+            289687.6972210754
+        )
+
+    """
+    for row in emissions_table:
+        yield (
+            lookup[row[2]],
             (db_name, "{}:{}".format(row[1], row[0])),
             "biosphere",
             row[5]
-        ) for row in emissions_table
-    ]
-    return data
+        )
 
 
 def relabel_exchanges(table, db_name):
