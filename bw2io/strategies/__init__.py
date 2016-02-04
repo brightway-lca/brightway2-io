940106a30e14f58710a61647e73812513059794d
Chris Mutel <cmutel@gmail.com>
2016-02-04 10:19:50 +0100

    Add __all__ to strategies

--- bw2io/strategies/__init__.py
+++ bw2io/strategies/__init__.py
@@ -2,13 +2,56 @@
 from __future__ import print_function, unicode_literals
 from eight import *
 
+__all__ = [
+    "add_activity_hash_code",
+    "assign_only_product_as_production",
+    "assign_single_product_as_activity",
+    "clean_integer_codes",
+    "create_composite_code",
+    "delete_exchanges_missing_activity",
+    "delete_ghost_exchanges",
+    "drop_unlinked",
+    "drop_unlinked_cfs",
+    "drop_unspecified_subcategories",
+    "es1_allocate_multioutput",
+    "es2_assign_only_product_with_amount_as_reference_product",
+    "link_biosphere_by_flow_uuid",
+    "link_internal_technosphere_by_composite_code",
+    "link_iterable_by_fields",
+    "link_technosphere_based_on_name_unit_location",
+    "link_technosphere_by_activity_hash",
+    "match_subcategories",
+    "migrate_datasets",
+    "migrate_exchanges",
+    "normalize_biosphere_categories",
+    "normalize_biosphere_names",
+    "normalize_simapro_biosphere_categories",
+    "normalize_simapro_biosphere_names",
+    "normalize_units",
+    "nuncertainty",
+    "remove_unnamed_parameters",
+    "remove_zero_amount_coproducts",
+    "remove_zero_amount_inputs_with_no_activity",
+    "set_biosphere_type",
+    "set_code_by_activity_hash",
+    "sp_allocate_products",
+    "special",
+    "split_simapro_name_geo",
+    "strip_biosphere_exc_locations",
+    "tupleize_categories",
+]
+
+
 from .biosphere import (
     drop_unspecified_subcategories,
     normalize_biosphere_categories,
     normalize_biosphere_names,
     strip_biosphere_exc_locations,
 )
-from .ecospold1_allocation import es1_allocate_multioutput, clean_integer_codes
+from .ecospold1_allocation import (
+    clean_integer_codes,
+    es1_allocate_multioutput,
+)
 from .ecospold2 import (
     assign_single_product_as_activity,
     create_composite_code,
