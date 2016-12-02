51e49af90976f6fd2cdd2823df42f954dcefadee
Chris Mutel <cmutel@gmail.com>
2016-12-02 13:30:40 +0100

    0.5.6. Improvements for handling SimaPro exports

--- bw2io/__init__.py
+++ bw2io/__init__.py
@@ -33,7 +33,7 @@ __all__ = [
     'UnlinkedData',
 ]
 
-__version__ = (0, 5, 5)
+__version__ = (0, 5, 6)
 
 
 from .package import BW2Package
--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import os
 
 setup(
     name='bw2io',
-    version="0.5.5",
+    version="0.5.6",
     packages=[
         'bw2io',
         'bw2io.data',
