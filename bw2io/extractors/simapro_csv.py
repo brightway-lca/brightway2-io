2e80d59152dc47e5e7e7e731b62de6f936ba4a43
Chris Mutel <cmutel@gmail.com>
2016-12-06 15:25:18 +0100

    Add more fields to SimaPro flexibility

--- bw2io/extractors/simapro_csv.py
+++ bw2io/extractors/simapro_csv.py
@@ -58,6 +58,7 @@ def to_number(obj):
         if "%" in obj:
             return float(obj.replace("%", "").strip()) / 100.
         try:
+            # Eval for simple expressions like "1/2"
             return float(eval(obj.replace(",", ".").strip()))
         except NameError:
             # Formula with a variable which isn't in scope - raises NameError
@@ -323,18 +324,24 @@ class SimaProCSVExtractor(object):
         7. uncert. param.
         8. comment
 
+        However, sometimes the value is in index 2, and the unit in index 3. Because why not! We assume default ordering unless we find a number in index 2.
+
         """
-        is_formula = not isinstance(to_number(line[3]), Number)
+        unit, amount = line[2], line[3]
+        if isinstance(to_number(line[2]), Number):
+            unit, amount = amount, unit
+
+        is_formula = not isinstance(to_number(amount), Number)
         if is_formula:
             ds = {
-                'formula': normalize_simapro_formulae(line[3], pm)
+                'formula': normalize_simapro_formulae(amount, pm)
             }
         else:
-            ds = cls.create_distribution(*line[3:8])
+            ds = cls.create_distribution(amount, *line[4:8])
         ds.update({
             'name': line[0],
             'categories': (category, line[1]),
-            'unit': line[2],
+            'unit': unit,
             'comment': "; ".join([x for x in line[8:] if x]),
             'type': 'biosphere',
         })
@@ -353,18 +360,24 @@ class SimaProCSVExtractor(object):
         6. uncert. param.
         7. comment
 
+        However, sometimes the value is in index 1, and the unit in index 2. Because why not! We assume default ordering unless we find a number in index 1.
+
         """
-        is_formula = not isinstance(to_number(line[2]), Number)
+        unit, amount = line[1], line[2]
+        if isinstance(to_number(line[1]), Number):
+            unit, amount = amount, unit
+
+        is_formula = not isinstance(to_number(amount), Number)
         if is_formula:
             ds = {
-                'formula': normalize_simapro_formulae(line[2], pm)
+                'formula': normalize_simapro_formulae(amount, pm)
             }
         else:
-            ds = cls.create_distribution(*line[2:7])
+            ds = cls.create_distribution(amount, *line[3:7])
         ds.update({
             'categories': (category,),
             'name': line[0],
-            'unit': line[1],
+            'unit': unit,
             'comment': "; ".join([x for x in line[7:] if x]),
             'type': ("substitution" if category == "Avoided products"
                       else 'technosphere'),
