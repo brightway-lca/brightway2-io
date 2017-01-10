06eee18e61e4cd2e0ae290fbed768417b893ac98
Chris Mutel <cmutel@gmail.com>
2017-01-10 11:41:55 +0100

    Add some common geodata

---
+++ tests/data/init_file.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+from __future__ import print_function, unicode_literals
+from eight import *
+
+from bw2io.data import get_valid_geonames
+
+
+def test_geodata():
+    assert 'WECC, US only' in get_valid_geonames()
