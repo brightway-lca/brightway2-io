c8b51ec463d93891e6f93824a22829264545abfb
Chris Mutel <cmutel@gmail.com>
2017-10-29 08:44:07 +0100

    Less aggressive replacing fields

--- tests/strategies/generic.py
+++ tests/strategies/generic.py
@@ -12,164 +12,189 @@ from bw2io.strategies import (
     tupleize_categories,
 )
 from bw2data import Database
-import copy
+from copy import deepcopy
 import numpy as np
-import unittest
+import pytest
 
 
-class GenericStrategiesTestCase(unittest.TestCase):
-    def test_tupleize_exchanges(self):
-        ds = [{
-            'exchanges': [{
-                'categories': ['resource', 'in ground'],
-            }]
-        }]
-        expected = [{
-            'exchanges': [{
-                'categories': (u'resource', u'in ground'),
-            }]
-        }]
-        self.assertEqual(
-            expected,
-            tupleize_categories(ds)
-        )
-
-    def test_tupleize_datasets(self):
-        ds = [{
+def test_tupleize_exchanges():
+    ds = [{
+        'exchanges': [{
             'categories': ['resource', 'in ground'],
         }]
-        expected = [{
+    }]
+    expected = [{
+        'exchanges': [{
             'categories': (u'resource', u'in ground'),
         }]
-        self.assertEqual(
-            expected,
-            tupleize_categories(ds)
-        )
+    }]
+    assert expected == tupleize_categories(ds)
 
-    def test_assign_only_product_as_production(self):
-        ds = [{'exchanges': [{
+def test_tupleize_datasets():
+    ds = [{
+        'categories': ['resource', 'in ground'],
+    }]
+    expected = [{
+        'categories': (u'resource', u'in ground'),
+    }]
+    assert expected == tupleize_categories(ds)
+
+def test_assign_only_product_as_production():
+    ds = [{
+        'exchanges': [{
             'amount': 2,
             'unit': 'kilogram',
             'name': 'foo',
             'type': 'production',
-        }]}]
-        expected = [{
-            'exchanges': [{
-                'amount': 2,
-                'unit': 'kilogram',
-                'name': 'foo',
-                'type': 'production',
-            }],
-            'name': 'foo',
-            'unit': 'kilogram',
-            'production amount': 2,
         }]
-        self.assertEqual(
-            assign_only_product_as_production(ds),
-            expected
-        )
-
-    def test_assign_only_product_already_reference_product(self):
-        ds = [{
-            'exchanges': [{
-                'amount': 2,
-                'unit': 'kilogram',
-                'name': 'foo',
-                'type': 'production',
-            }],
-            'reference product': 'foo',
-        }]
-        self.assertEqual(
-            ds,
-            assign_only_product_as_production(copy.deepcopy(ds)),
-        )
-
-    def test_assign_only_product_no_products(self):
-        ds = [{'name': 'hi'}]
-        self.assertEqual(
-            ds,
-            assign_only_product_as_production(copy.deepcopy(ds)),
-        )
-
-    def test_assign_only_product_multiple_products(self):
-        ds = [{'exchanges': [{
+    }]
+    expected = [{
+        'exchanges': [{
             'amount': 2,
             'unit': 'kilogram',
             'name': 'foo',
             'type': 'production',
-        }, {
-            'amount': 3,
+        }],
+        'name': 'foo',
+        'reference product': 'foo',
+        'unit': 'kilogram',
+        'production amount': 2,
+    }]
+    assert assign_only_product_as_production(ds) == expected
+
+def test_assign_only_product_no_name():
+    ds = [{
+        'exchanges': [{
+            'amount': 2,
+            'unit': 'kilogram',
+            'name': '',
+            'type': 'production',
+        }]
+    }]
+    with pytest.raises(AssertionError):
+        assign_only_product_as_production(ds)
+    ds = [{
+        'exchanges': [{
+            'amount': 2,
+            'unit': 'kilogram',
+            'type': 'production',
+        }]
+    }]
+    with pytest.raises(KeyError):
+        assign_only_product_as_production(ds)
+
+def test_assign_only_product_leave_fields():
+    ds = [{
+        'exchanges': [{
+            'amount': 2,
             'unit': 'kilogram',
             'name': 'foo',
             'type': 'production',
-        }]}]
-        self.assertEqual(
-            ds,
-            assign_only_product_as_production(copy.deepcopy(ds)),
-        )
+        }],
+        'name': 'bar',
+        'unit': 'bar',
+        'production amount': 12,
+    }]
+    expected = [{
+        'exchanges': [{
+            'amount': 2,
+            'unit': 'kilogram',
+            'name': 'foo',
+            'type': 'production',
+        }],
+        'name': 'bar',
+        'reference product': 'foo',
+        'unit': 'bar',
+        'production amount': 2,
+    }]
+    assert assign_only_product_as_production(ds) == expected
 
-    def test_set_code_by_activity_hash(self):
-        ds = [{'name': 'hi'}]
-        expected = [{
-            'code': '49f68a5c8493ec2c0bf489821c21fc3b',
-            'name': 'hi',
-        }]
-        self.assertEqual(
-            set_code_by_activity_hash(ds),
-            expected
-        )
+def test_assign_only_product_already_reference_product():
+    ds = [{
+        'exchanges': [{
+            'amount': 2,
+            'unit': 'kilogram',
+            'name': 'foo',
+            'type': 'production',
+        }],
+        'reference product': 'bar',
+    }]
+    assert ds == assign_only_product_as_production(deepcopy(ds))
 
-    def test_set_code_by_activity_hash_overwrite(self):
-        ds = [{
-            'code': 'foo',
-            'name': 'hi'
-        }]
-        expected = [{
-            'code': '49f68a5c8493ec2c0bf489821c21fc3b',
-            'name': 'hi',
-        }]
-        assert set_code_by_activity_hash(ds)[0]['code'] == 'foo'
-        assert set_code_by_activity_hash(ds, True) == expected
+def test_assign_only_product_no_products():
+    ds = [{'name': 'hi'}]
+    assert ds == assign_only_product_as_production(deepcopy(ds))
 
-    # def test_link_technosphere_internal(self):
-    #     unlinked = [{
-    #         'exchanges': [{
-    #             'name': 'foo',
-    #             'type': 'technosphere',
-    #         }]
-    #     }]
-    #     other = [{
-    #         'name': 'foo',
-    #         'type': 'process',
-    #         'database': 'db',
-    #         'code': 'first'
-    #     }]
-    #     expected = [{
-    #         'exchanges': [{
-    #             'name': 'foo',
-    #             'type': 'technosphere',
-    #             'input': ('db', 'first')
-    #         }]
-    #     }]
-    #     self.assertEqual(
-    #         expected,
-    #         link_technosphere_by_activity_hash(unlinked, other)
-    #     )
+def test_assign_only_product_multiple_products():
+    ds = [{'exchanges': [{
+        'amount': 2,
+        'unit': 'kilogram',
+        'name': 'foo',
+        'type': 'production',
+    }, {
+        'amount': 3,
+        'unit': 'kilogram',
+        'name': 'foo',
+        'type': 'production',
+    }]}]
+    assert ds == assign_only_product_as_production(deepcopy(ds))
 
-    def test_link_technosphere_wrong_type(self):
-        pass
+def test_set_code_by_activity_hash():
+    ds = [{'name': 'hi'}]
+    expected = [{
+        'code': '49f68a5c8493ec2c0bf489821c21fc3b',
+        'name': 'hi',
+    }]
+    assert set_code_by_activity_hash(ds) == expected
 
-    def test_link_technosphere_fields(self):
-        pass
+def test_set_code_by_activity_hash_overwrite():
+    ds = [{
+        'code': 'foo',
+        'name': 'hi'
+    }]
+    expected = [{
+        'code': '49f68a5c8493ec2c0bf489821c21fc3b',
+        'name': 'hi',
+    }]
+    assert set_code_by_activity_hash(ds)[0]['code'] == 'foo'
+    assert set_code_by_activity_hash(ds, True) == expected
 
-    def test_link_technosphere_external(self):
-        pass
+# def test_link_technosphere_internal():
+#     unlinked = [{
+#         'exchanges': [{
+#             'name': 'foo',
+#             'type': 'technosphere',
+#         }]
+#     }]
+#     other = [{
+#         'name': 'foo',
+#         'type': 'process',
+#         'database': 'db',
+#         'code': 'first'
+#     }]
+#     expected = [{
+#         'exchanges': [{
+#             'name': 'foo',
+#             'type': 'technosphere',
+#             'input': ('db', 'first')
+#         }]
+#     }]
+#     self.assertEqual(
+#         expected,
+#         link_technosphere_by_activity_hash(unlinked, other)
+#     )
 
-    def test_link_technosphere_external_untyped(self):
-        pass
+def test_link_technosphere_wrong_type():
+    pass
 
+def test_link_technosphere_fields():
+    pass
 
+def test_link_technosphere_external():
+    pass
 
+def test_link_technosphere_external_untyped():
+    pass
 
 def test_convert_uncertainty_types_to_integers():
     data = [{'exchanges': [
@@ -184,7 +209,6 @@ def test_convert_uncertainty_types_to_integers():
     ]}]
     result = convert_uncertainty_types_to_integers(data)
     assert result == expected
-
 
 def test_drop_falsey_uncertainty_fields_but_keep_zeros():
     data = [{'exchanges': [
