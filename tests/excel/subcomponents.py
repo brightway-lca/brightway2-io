3c629ff56d667f8f8abd73ed5107b68f4929b513
Chris Mutel <cmutel@gmail.com>
2017-10-30 10:02:14 +0100

    More work on excel tests

--- tests/excel/subcomponents.py
+++ tests/excel/subcomponents.py
@@ -6,62 +6,9 @@ from bw2data import Database
 from bw2data.parameters import *
 from bw2data.tests import bw2test
 from bw2io import ExcelImporter
-from bw2io.importers.excel import (
-    csv_restore_tuples,
-    csv_restore_booleans,
-    csv_numerize,
-    csv_drop_unknown,
-    csv_add_missing_exchanges_section,
-    normalize_units,
-    set_code_by_activity_hash,
-    assign_only_product_as_production,
-    link_technosphere_by_activity_hash,
-    drop_falsey_uncertainty_fields_but_keep_zeros,
-    convert_uncertainty_types_to_integers,
-)
 from copy import deepcopy
-import os
 import pytest
 
-EXCEL_FIXTURES_DIR = os.path.join(os.path.dirname(__file__), "fixtures", "excel")
-
-
-@bw2test
-def test_excel_import():
-    exc = ExcelImporter(os.path.join(EXCEL_FIXTURES_DIR, "sample_activities_with_variables.xlsx"))
-    exc.strategies = [
-        csv_restore_tuples,
-        csv_restore_booleans,
-        csv_numerize,
-        csv_drop_unknown,
-        csv_add_missing_exchanges_section,
-        normalize_units,
-        set_code_by_activity_hash,
-        assign_only_product_as_production,
-        link_technosphere_by_activity_hash,
-        drop_falsey_uncertainty_fields_but_keep_zeros,
-        convert_uncertainty_types_to_integers,
-    ]
-    assert exc.project_parameters
-    assert exc.database_parameters
-    assert exc.metadata
-    assert exc.data
-    exc.apply_strategies()
-    exc.match_database(fields=["name"])
-    exc.write_project_parameters()
-    exc.write_database(activate_parameters=True)
-
-    for o in ProjectParameter.select():
-        print(o.dict)
-    for o in DatabaseParameter.select():
-        print(o.dict)
-    for o in ActivityParameter.select():
-        print(o.dict)
-    for ds in Database("PCB"):
-        for exc in ds.exchanges():
-            print(exc)
-
-    # raise ValueError
 
 @pytest.fixture
 def no_init(monkeypatch):
@@ -171,6 +118,59 @@ def test_get_activity(no_init):
             'amount': 0.6,
             'formula': 'PCB_cap_mass_film + PCB_cap_mass_SMD + PCB_cap_mass_Tantalum'}},
         'type': 'process',
+        'unit': 'kilogram',
+        'worksheet name': 'a'}
+    assert ei.get_activity('a', given) == expected
+
+@bw2test
+def test_get_activity_metadata_indexing(no_init):
+    given = [
+        ['Activity', 'mounted printed circuit board', '', '', '', '', '', '', '', '', '', '', '', ''],
+        ['arbitrary', 'metadata', '', '', '', '', '', '', '', '', '', '', '', ''],
+        ['unit', 'kilogram', '', '', '', '', '', '', '', '', '', '', '', ''],
+        ['', '', '', '', '', '', '', '', '', '', '', '', '', ''],
+        ['Parameters', '', '', '', '', '', '', '', '', '', '', '', '', ''],
+    ]
+    ei = ExcelImporter()
+    ei.db_name = 'db'
+    expected = {
+        'arbitrary': 'metadata',
+        'database': 'db',
+        'name': 'mounted printed circuit board',
+        'exchanges': [],
+        'unit': 'kilogram',
+        'worksheet name': 'a'}
+    assert ei.get_activity('a', given) == expected
+
+    given = [
+        ['Activity', 'mounted printed circuit board', '', '', '', '', '', '', '', '', '', '', '', ''],
+        ['arbitrary', 'metadata', '', '', '', '', '', '', '', '', '', '', '', ''],
+        ['unit', 'kilogram', '', '', '', '', '', '', '', '', '', '', '', ''],
+        ['', '', '', '', '', '', '', '', '', '', '', '', '', ''],
+    ]
+    ei = ExcelImporter()
+    ei.db_name = 'db'
+    expected = {
+        'arbitrary': 'metadata',
+        'database': 'db',
+        'name': 'mounted printed circuit board',
+        'exchanges': [],
+        'unit': 'kilogram',
+        'worksheet name': 'a'}
+    assert ei.get_activity('a', given) == expected
+
+    given = [
+        ['Activity', 'mounted printed circuit board', '', '', '', '', '', '', '', '', '', '', '', ''],
+        ['arbitrary', 'metadata', '', '', '', '', '', '', '', '', '', '', '', ''],
+        ['unit', 'kilogram', '', '', '', '', '', '', '', '', '', '', '', ''],
+    ]
+    ei = ExcelImporter()
+    ei.db_name = 'db'
+    expected = {
+        'arbitrary': 'metadata',
+        'database': 'db',
+        'name': 'mounted printed circuit board',
+        'exchanges': [],
         'unit': 'kilogram',
         'worksheet name': 'a'}
     assert ei.get_activity('a', given) == expected
