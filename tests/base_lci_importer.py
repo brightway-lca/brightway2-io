da54761489a3f4733186be933760780628348282
Chris Mutel <cmutel@gmail.com>
2017-11-02 15:17:00 +0100

    More test coverage

--- tests/base_lci_importer.py
+++ tests/base_lci_importer.py
@@ -6,23 +6,12 @@ from bw2data import Database
 from bw2data.parameters import *
 from bw2data.tests import bw2test
 from bw2io import ExcelImporter
+from bw2io.errors import NonuniqueCode, WrongDatabase
 from bw2io.importers.base_lci import LCIImporter
-import pytest
+from copy import deepcopy
 import numpy as np
+import pytest
 
-### Tests TODO
-# Activity parameters without group names
-# Activity parameters with group names
-# Activity parameters - multiple activities with same group name
-# Test delete database parameters
-# Test skip delete if parameters are None
-# Test update database parameters
-# Delete stale activity parameters with wrong (old) group
-# Test delete activity parameters (right group, delete_existing)
-# Test update activity parameters
-# Test error formatting for wrongdatabase
-# Test error formatting for nonuniquecode
-# Test database update existing data
 
 DATA = [
          {'arbitrary': 'metadata',
@@ -102,8 +91,8 @@ DB = [{
 def lci():
     obj = LCIImporter("PCB")
     obj.project_parameters = [{'amount': 0.25, 'name': 'PCB_area'}]
-    obj.data = DATA
-    obj.database_parameters = DB
+    obj.data = deepcopy(DATA)
+    obj.database_parameters = deepcopy(DB)
     return obj
 
 
@@ -160,3 +149,221 @@ def test_delete_project_parameters(lci):
     d.project_parameters = []
     d.write_project_parameters()
     assert not ProjectParameter.select().count()
+
+def test_update_project_parameters(lci):
+    lci.write_project_parameters()
+    assert ProjectParameter.get(name="PCB_area").amount == 0.25
+    obj = LCIImporter("PCB")
+    obj.project_parameters = [{'amount': 5, 'name': 'PCB_area'}]
+    obj.write_project_parameters()
+    assert ProjectParameter.get(name="PCB_area").amount == 5
+
+def test_no_delete_database_parameters(lci):
+    pass
+
+def test_delete_database_parameters(lci):
+    pass
+
+def test_update_database_parameters(lci):
+    pass
+
+@bw2test
+def test_activity_parameters_with_group_name():
+    DATA = [{
+        'code': 'A',
+        'database': 'db',
+        'exchanges': [{
+            'amount': 0.0,
+            'input': ('db', 'A'),
+            'location': 'GLO',
+            'name': 'mounted printed circuit board',
+            'type': 'production',
+            'unit': 'kilogram'
+        }],
+        'location': 'GLO',
+        'name': 'mounted printed circuit board',
+        'parameters': {'something_something': {'amount': 0.8, 'group': 'g'}},
+        'type': 'process',
+        'unit': 'kilogram',
+    }]
+    obj = LCIImporter("db")
+    obj.data = deepcopy(DATA)
+    obj.write_database(activate_parameters=True)
+    assert Group.select().where(Group.name == 'g').count() == 1
+    assert Group.select().count() == 1
+    assert ActivityParameter.get(name='something_something').amount == 0.8
+
+@bw2test
+def test_activity_multiple_activities_same_group_name():
+    DATA = [{
+        'code': 'A',
+        'database': 'db',
+        'exchanges': [{
+            'amount': 0.0,
+            'input': ('db', 'A'),
+            'location': 'GLO',
+            'type': 'production',
+        }],
+        'location': 'GLO',
+        'name': 'mounted printed circuit board',
+        'parameters': {'something_something': {'amount': 0.8, 'group': 'g'}},
+        'type': 'process',
+        'unit': 'kilogram',
+    }, {
+        'code': 'B',
+        'database': 'db',
+        'exchanges': [{
+            'amount': 0.0,
+            'input': ('db', 'B'),
+            'location': 'GLO',
+            'type': 'production',
+        }],
+        'location': 'GLO',
+        'name': 'bla bla',
+        'parameters': {'something_else': {'amount': 0.2, 'group': 'g'}},
+        'type': 'process',
+        'unit': 'kilogram',
+    }]
+    obj = LCIImporter("db")
+    obj.data = deepcopy(DATA)
+    obj.write_database(activate_parameters=True)
+    assert Group.select().where(Group.name == 'g').count() == 1
+    assert Group.select().count() == 1
+    assert ActivityParameter.get(name='something_something').amount == 0.8
+    assert ActivityParameter.get(name='something_else').amount == 0.2
+
+@bw2test
+def test_wrongdatabase_error_code():
+    lci = LCIImporter("woo")
+    data = [
+         {'code': '32aa5ab78beda5b8c8efbc89587de7a5',
+          'database': 'woo',
+          'exchanges': [],
+          'location': 'GLO',
+          'name': 'mounted printed circuit board',
+          'parameters': {},
+          'type': 'process',
+          'unit': 'kilogram'},
+         {'code': '45cb34db4147e510a2561cceec541f6b',
+          'database': 'PCB',
+          'exchanges': [],
+          'location': 'GLO',
+          'parameters': {},
+          'name': 'unmounted printed circuit board',
+          'type': 'process',
+          'unit': 'square meter'}
+    ]
+    with pytest.raises(WrongDatabase):
+        lci.write_database(data)
+
+@bw2test
+def test_nonuniquecode_error_code():
+    lci = LCIImporter("woo")
+    data = [
+         {'code': '32aa5ab78beda5b8c8efbc89587de7a5',
+          'database': 'woo',
+          'exchanges': [],
+          'location': 'GLO',
+          'name': 'mounted printed circuit board',
+          'parameters': {},
+          'type': 'process',
+          'unit': 'kilogram'},
+         {'code': '32aa5ab78beda5b8c8efbc89587de7a5',
+          'database': 'woo',
+          'exchanges': [],
+          'location': 'GLO',
+          'parameters': {},
+          'name': 'unmounted printed circuit board',
+          'type': 'process',
+          'unit': 'square meter'}
+    ]
+    with pytest.raises(NonuniqueCode):
+        lci.write_database(data)
+
+def test_database_update_existing_data(lci):
+    pass
+
+@pytest.mark.skip("Don't update exchanges yet")
+def test_update_activity_parameters(lci):
+    lci.write_project_parameters()
+    lci.write_database(activate_parameters=True)
+    assert ActivityParameter.get(name='PCB_mass_total').formula == \
+        'PCB_cap_mass_film + PCB_cap_mass_SMD + PCB_cap_mass_Tantalum'
+    new = [{
+        'code': '32aa5ab78beda5b8c8efbc89587de7a5',
+        'database': 'PCB',
+        'exchanges': [],
+        'location': 'GLO',
+        'name': 'mounted printed circuit board',
+        'parameters': {'PCB_mass_total': {
+            'amount': 0.6,
+            'formula': 'PCB_cap_mass_film + 2'}
+        },
+        'type': 'process',
+        'unit': 'kilogram'},
+    ]
+    obj = LCIImporter("PCB")
+    obj.data = deepcopy(new)
+    obj.write_database(activate_parameters=True)
+    assert ActivityParameter.get(name='PCB_mass_total').formula == \
+        'PCB_cap_mass_film + 2'
+
+def test_activity_parameters_delete_old_groupname(lci):
+    lci.write_project_parameters()
+    lci.write_database(activate_parameters=True)
+    assert ActivityParameter.select().where(
+        ActivityParameter.group == 'PCB:32aa5ab78beda5b8c8efbc89587de7a5'
+    ).count()
+    assert not ActivityParameter.select().where(
+        ActivityParameter.group == 'some other group'
+    ).count()
+    new = [{
+        'code': '32aa5ab78beda5b8c8efbc89587de7a5',
+        'database': 'PCB',
+        'exchanges': [],
+        'location': 'GLO',
+        'name': 'mounted printed circuit board',
+        'parameters': {'PCB_mass_total': {
+            'amount': 0.6,
+            'group': 'some other group',
+            'formula': 'PCB_cap_mass_film + 2'}
+        },
+        'type': 'process',
+        'unit': 'kilogram'},
+    ]
+    obj = LCIImporter("PCB")
+    obj.data = deepcopy(new)
+    obj.write_database(activate_parameters=True)
+    assert not ActivityParameter.select().where(
+        ActivityParameter.group == 'PCB:32aa5ab78beda5b8c8efbc89587de7a5'
+    ).count()
+
+def test_delete_activity_parameters_delete_existing(lci):
+    lci.write_project_parameters()
+    lci.write_database(activate_parameters=True)
+    new = [
+         {'code': '32aa5ab78beda5b8c8efbc89587de7a5',
+          'database': 'PCB',
+          'exchanges': [],
+          'location': 'GLO',
+          'name': 'mounted printed circuit board',
+          'parameters': {},
+          'type': 'process',
+          'unit': 'kilogram'},
+         {'code': '45cb34db4147e510a2561cceec541f6b',
+          'database': 'PCB',
+          'exchanges': [],
+          'location': 'GLO',
+          'parameters': {},
+          'name': 'unmounted printed circuit board',
+          'type': 'process',
+          'unit': 'square meter'}
+    ]
+    obj = LCIImporter("PCB")
+    obj.data = deepcopy(new)
+    obj.write_database(activate_parameters=False, delete_existing=True)
+    assert ActivityParameter.select().count()
+    obj = LCIImporter("PCB")
+    obj.data = new
+    obj.write_database(activate_parameters=True, delete_existing=True)
+    assert not ActivityParameter.select().count()
