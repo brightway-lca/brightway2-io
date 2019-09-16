89eff47c79523d7530f318ccb8f2f99200a5741a
Chris Mutel <cmutel@gmail.com>
2019-09-16 13:53:19 +0200

    0.7.7

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import os
 
 setup(
     name='bw2io',
-    version="0.7.6",
+    version="0.7.7",
     packages=[
         'bw2io',
         'bw2io.data',
@@ -21,8 +21,8 @@ setup(
     author_email="cmutel@gmail.com",
     license="BSD 3-clause",
     install_requires=[
-        "bw2calc",
-        "bw2data>=3.3",
+        "bw2calc>=1.7.4",
+        "bw2data>=3.5.1",
         "lxml",
         "numpy",
         "psutil",
