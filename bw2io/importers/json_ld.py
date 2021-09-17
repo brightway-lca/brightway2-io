6149c09117c20956bed1f48fc118989eb288cdcb
Chris Mutel <cmutel@gmail.com>
2021-09-17 09:23:14 +0200

    Clean up merge artifacts

--- bw2io/importers/json_ld.py
+++ bw2io/importers/json_ld.py
@@ -1,16 +1,12 @@
 from ..extractors.json_ld import JSONLDExtractor
 from ..strategies import (
-<<<<<<< HEAD
+    add_database_name,
+    json_ld_add_activity_unit,
+    json_ld_convert_db_dict_into_list,
+    json_ld_get_activities_list_from_rawdata,
     json_ld_get_normalized_exchange_locations,
     json_ld_get_normalized_exchange_units,
-    json_ld_get_activities_list_from_rawdata,
-    json_ld_add_activity_unit,
     json_ld_rename_metadata_fields,
-=======
->>>>>>> 06f7e1038eb70eaae6a169878697f6589f6a979f
-    add_database_name,
-    json_ld_convert_db_dict_into_list,
-    json_ld_get_normalized_exchange_locations,
     link_iterable_by_fields,
     link_technosphere_by_activity_hash,
     normalize_units,
@@ -33,13 +29,10 @@ class JSONLDImporter(LCIImporter):
     def __init__(self, dirpath):
         self.strategies = [
             json_ld_get_normalized_exchange_locations,
-<<<<<<< HEAD
             json_ld_get_normalized_exchange_units,
             json_ld_get_activities_list_from_rawdata,
             json_ld_add_activity_unit,
             json_ld_rename_metadata_fields,
-=======
             json_ld_convert_db_dict_into_list,
->>>>>>> 06f7e1038eb70eaae6a169878697f6589f6a979f
         ]
         self.data = self.extractor.extract(dirpath)
