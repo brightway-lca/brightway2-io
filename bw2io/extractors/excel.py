20f7d11b89540d83fea0516c7228413b121b933b
Chris Mutel <cmutel@gmail.com>
2021-09-29 16:21:30 +0200

    Port PR #101 to legacy branch

--- bw2io/extractors/excel.py
+++ bw2io/extractors/excel.py
@@ -4,7 +4,7 @@ from openpyxl import load_workbook
 
 
 def get_cell_value_handle_error(cell):
-    if cell.data_type == 'e':
+    if cell.data_type == "e":
         # Error type
         return None
     else:
@@ -15,11 +15,13 @@ class ExcelExtractor(object):
     @classmethod
     def extract(cls, filepath):
         assert os.path.exists(filepath), "Can't file file at path {}".format(filepath)
-        wb = load_workbook(filepath, data_only=True)
+        wb = load_workbook(filepath, data_only=True, read_only=True)
         return [(name, cls.extract_sheet(wb, name)) for name in wb.sheetnames]
 
     @classmethod
     def extract_sheet(cls, wb, name, strip=True):
         ws = wb[name]
         _ = lambda x: x.strip() if (strip and hasattr(x, "strip")) else x
-        return [[_(get_cell_value_handle_error(ws.cell(row=row + 1, column=col + 1))) for col in range(ws.max_column)] for row in range(ws.max_row)]
+        return [
+            [_(get_cell_value_handle_error(cell)) for cell in row] for row in ws.rows
+        ]
