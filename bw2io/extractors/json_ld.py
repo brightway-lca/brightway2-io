bfeac56a959cbd4b28469cceecb65ed5fb20665f
Chris Mutel <cmutel@gmail.com>
2021-10-01 18:50:14 +0200

    Specify encoding of JSON files when extracting

--- bw2io/extractors/json_ld.py
+++ bw2io/extractors/json_ld.py
@@ -11,20 +11,6 @@ DIRECTORIES_TO_IGNORE = {
     "bin",
 }
 
-IGNORE_ME = lambda x: x.startswith(".")
-
-
-def walk_dir(dirpath):
-    return [
-        (os.path.splitext(file)[0], os.path.join(dirpath, file))
-        for file in os.listdir(dirpath)
-        if os.path.isfile(os.path.join(dirpath, file)) and is_json_file(file)
-    ]
-
-
-def is_json_file(filepath):
-    return str(filepath).lower().endswith("json")
-
 
 class JSONLDExtractor(object):
     @classmethod
@@ -48,10 +34,11 @@ class JSONLDExtractor(object):
                 directory.name: dict(
                     sorted(
                         [
-                            (fp.stem, json.load(open(fp)))
+                            (fp.stem, json.load(open(fp, encoding='utf-8')))
                             for fp in directory.iterdir()
                             if fp.name not in FILES_TO_IGNORE
                             and not fp.name.startswith(".")
+                            and "json" in fp.suffix.lower()
                         ]
                     )
                 )
