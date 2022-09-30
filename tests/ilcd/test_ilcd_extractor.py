9d4aa11d4a0bbb143762bbe30626ee8b0069dc25
Benjamin W. Portner <benjamin.portner@bauhaus-luftfahrt.net>
2022-09-30 10:58:52 +0200

    improve test for ilcd xml value getter

--- tests/ilcd/test_ilcd_extractor.py
+++ tests/ilcd/test_ilcd_extractor.py
@@ -1,29 +1,32 @@
-from bw2io.extractors.ilcd import extract_zip
+from bw2io.extractors.ilcd import extract_zip, get_xml_value, xpaths
 from pathlib import Path
 from lxml.etree import _Element
 
+example_file = Path(".").absolute().parent.parent / "bw2io/data/examples/ilcd-example.zip"
+
 def test_extract_zip():
-    file = Path(".").absolute().parent.parent / "bw2io/data/examples/ilcd-example.zip"
-    trees = extract_zip(file)
+
+    trees = extract_zip(example_file)
 
     # assure completeness
     assert len(trees) == 1240
 
     # assure that all return values are etrees
     assert all([isinstance(t, _Element) for t in trees.values()])
-    pass
+
+    return trees
 
 
-def test_parser():
-    fp_process = "/Users/michael.baer/Dropbox (On)/Michaels Data/ILCD-hackaton/ILCD/processes/d2fe899e-7fc0-49d3-a7cc-bbf8cad5439a_00.00.001.xml"
-    fp_flows = "/Users/michael.baer/Dropbox (On)/Michaels Data/ILCD-hackaton/ILCD/flows/0a51e24a-6201-47bb-b8f2-eb52bca60c83_03.00.004.xml"
-    tree_object = etree.parse(open(fp_flows, encoding="utf-8"))
-
+def test_xml_value_getter():
+    trees = extract_zip(example_file)
+    tree_object = list(trees.values())[0]
     general_namespace = {"n": "http://lca.jrc.it/ILCD/Flow"}
     namespaces = {"common": "http://lca.jrc.it/ILCD/Common"}
     namespaces.update(general_namespace)
     xpath_str = xpaths["value"]
-    get_xml_value(tree_object, xpath_str, general_namespace, namespaces)
+    v = get_xml_value(tree_object, xpath_str, general_namespace, namespaces)
+    assert v == '1.0'
 
 if __name__ == "__main__":
     test_extract_zip()
+    test_xml_value_getter()
