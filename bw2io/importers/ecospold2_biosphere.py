aabd2bd36820e84a20fa02d659c0cbdafe2f55be
Chris Mutel <cmutel@gmail.com>
2022-10-13 20:08:03 +0200

    Switch to 3.9 flow list

--- bw2io/importers/ecospold2_biosphere.py
+++ bw2io/importers/ecospold2_biosphere.py
@@ -21,16 +21,16 @@ EMISSIONS_CATEGORIES = {
 class Ecospold2BiosphereImporter(LCIImporter):
     format = "Ecoinvent XML"
 
-    def __init__(self, name="biosphere3"):
+    def __init__(self, name="biosphere3", version="3.9"):
         self.db_name = name
-        self.data = self.extract()
+        self.data = self.extract(version)
         self.strategies = [
             normalize_units,
             drop_unspecified_subcategories,
             ensure_categories_are_tuples,
         ]
 
-    def extract(self):
+    def extract(self, version):
         def extract_flow_data(o):
             ds = {
                 "categories": (
@@ -51,11 +51,12 @@ class Ecospold2BiosphereImporter(LCIImporter):
 
         lci_dirpath = os.path.join(os.path.dirname(__file__), "..", "data", "lci")
 
-        fp = os.path.join(lci_dirpath, "ecoinvent elementary flows 3.8.xml")
+        fp = os.path.join(lci_dirpath, f"ecoinvent elementary flows {version}.xml")
         root = objectify.parse(open(fp, encoding="utf-8")).getroot()
         flow_data = recursive_str_to_unicode(
             [extract_flow_data(ds) for ds in root.iterchildren()]
         )
 
-        previous = os.path.join(lci_dirpath, "previous elementary flows.json")
-        return flow_data + json.load(open(previous))
+        # previous = os.path.join(lci_dirpath, "previous elementary flows.json")
+        # return flow_data + json.load(open(previous))
+        return flow_data
