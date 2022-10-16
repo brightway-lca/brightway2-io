3cd7599db8cbc787dd673a0b0361ef5f903d1d39
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-16 13:39:54 +0000

    Adding tarfile member sanitization to extractall()

--- bw2io/backup.py
+++ bw2io/backup.py
@@ -74,7 +74,26 @@ def restore_project_directory(fp):
     project_name = get_project_name(fp)
 
     with tarfile.open(fp, "r|gz") as tar:
-        tar.extractall(projects._base_data_dir)
+        def is_within_directory(directory, target):
+            
+            abs_directory = os.path.abspath(directory)
+            abs_target = os.path.abspath(target)
+        
+            prefix = os.path.commonprefix([abs_directory, abs_target])
+            
+            return prefix == abs_directory
+        
+        def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+        
+            for member in tar.getmembers():
+                member_path = os.path.join(path, member.name)
+                if not is_within_directory(path, member_path):
+                    raise Exception("Attempted Path Traversal in Tar File")
+        
+            tar.extractall(path, members, numeric_owner=numeric_owner) 
+            
+        
+        safe_extract(tar, projects._base_data_dir)
 
     _current = projects.current
     projects.set_current(project_name, update=False)
