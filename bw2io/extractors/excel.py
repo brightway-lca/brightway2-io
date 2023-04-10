50046593bec8c48322696e96e44777fc7202ae87
Cerebrovinny <vinny.cardoso96@gmail.com>
2023-04-09 21:21:28 -0300

    improvements for excel

--- bw2io/extractors/excel.py
+++ bw2io/extractors/excel.py
@@ -5,17 +5,27 @@ from openpyxl import load_workbook
 
 def get_cell_value_handle_error(cell):
     """
-    Get the value of a cell, handling error types.
+    Retrieve the value of a given cell and handle error types.
 
-    Parameters
-    ----------
-    cell : openpyxl.cell.cell.cell
+    Parameters:
+    -----------
+    cell : openpyxl.cell.cell.Cell
         The cell to retrieve the value from.
 
-    Returns
-    -------
+    Returns:
+    --------
     object
         The value of the cell, or None if the cell has an error type.
+
+    Examples:
+    ---------
+    >>> from openpyxl import Workbook
+    >>> wb = Workbook()
+    >>> ws = wb.active
+    >>> ws["A1"] = "hello"
+    >>> assert get_cell_value_handle_error(ws["A1"]) == "hello"
+    >>> ws["B1"] = "=1/0"
+    >>> assert get_cell_value_handle_error(ws["B1"]) == None
     """
     if cell.data_type == "e":
         # Error type
@@ -26,17 +36,46 @@ def get_cell_value_handle_error(cell):
 
 class ExcelExtractor(object):
     """
-    Extract data from an Excel file.
-
+    A class used to extract data from an Excel file.
+    
     Parameters
     ----------
     object : type
-        The parent object for the ExcelExtractor class
+        The parent object for the ExcelExtractor class.
 
     Returns
     -------
     object
-        An instance of the class
+        An instance of the class.
+
+    See Also
+    --------
+    openpyxl.load_workbook : Load a workbook from a file.
+
+    Notes
+    -----
+    This class requires the openpyxl package to be installed.
+
+    Raises
+    ------
+    AssertionError
+        If the file at 'filepath' does not exist.
+
+    Parameters
+    ----------
+    filepath : str
+        The path to the Excel file.
+
+    Returns
+    -------
+    list
+        A list of tuples containing the name of each sheet in the file and the data from each sheet.
+
+    Examples
+    --------
+    >>> extractor = ExcelExtractor()
+    >>> filepath = 'example.xlsx'
+    >>> data = extractor.extract(filepath)
     """
     @classmethod
     def extract(cls, filepath):
@@ -52,7 +91,7 @@ class ExcelExtractor(object):
         -------
         list
             A list of tuples containing the name of each sheet in the file and the data from each sheet.
-        
+
         Raises
         ------
         AssertionError
@@ -67,21 +106,29 @@ class ExcelExtractor(object):
     @classmethod
     def extract_sheet(cls, wb, name, strip=True):
         """
-        Extract data from a single sheet in an Excel file.
+        Extract data from a single sheet in an Excel workbook.
 
         Parameters
         ----------
         wb : openpyxl.workbook.Workbook
-            The workbook object with the sheet to extract data from.
-        name : str
-            The name of the sheet to extract data from.
-        strip : bool, optional
-            If True, strip whitespace from cell values, by default True.
+            The workbook containing the sheet to extract.
+        sheetname : str
+            The name of the sheet to extract.
 
         Returns
         -------
         list
-            A list of lists containing the data from each cell in the sheet.
+            A list of lists containing the data from the sheet.
+
+        Notes
+        -----
+        This method is called by the 'extract' method to extract the data from each sheet in the workbook.
+
+        Examples
+        --------
+        >>> wb = openpyxl.load_workbook('example.xlsx')
+        >>> sheetname = 'Sheet1'
+        >>> data = ExcelExtractor.extract_sheet(wb, sheetname)
         """
         ws = wb[name]
         _ = lambda x: x.strip() if (strip and hasattr(x, "strip")) else x
