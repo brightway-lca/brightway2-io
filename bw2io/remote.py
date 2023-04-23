165093e57c546e4912bf8888cf1d9b26d0602336
Chris Mutel <cmutel@gmail.com>
2023-04-23 19:01:32 +0200

    Try again when downloading projects

--- bw2io/remote.py
+++ bw2io/remote.py
@@ -44,6 +44,7 @@ def install_project(
     projects_config: Optional[dict] = get_projects(),
     url: Optional[str] = "https://files.brightway.dev/",
     overwrite_existing: Optional[bool] = False,
+    __recursive: bool | None = False
 ):
     """
     Install an existing Brightway project archive.
@@ -62,6 +63,8 @@ def install_project(
         The URL, with trailing slash ``/``, where the file can be found.
     overwrite_existing: bool, optional
         Allow overwriting an existing project
+    __recursive : bool
+        Internal flag used to determine if this function has errored out already
 
     Returns
     -------
@@ -79,6 +82,21 @@ def install_project(
             url=url + filename, filename=filename, dirpath=cache_dir
         )
 
-    return restore_project_directory(
-        fp=fp, project_name=project_name, overwrite_existing=overwrite_existing
-    )
+    try:
+        return restore_project_directory(
+            fp=fp, project_name=project_name, overwrite_existing=overwrite_existing
+        )
+    except EOFError:
+        # Corrupt or incomplete zip archive
+        fp.unlink()
+        if __recursive:
+            raise OSError("Multiple errors trying to download and extract this file. Better luck tomorrow?")
+        else:
+            return install_project(
+                project_key=project_key,
+                project_name=project_name,
+                projects_config=projects_config,
+                url=url,
+                overwrite_existing=overwrite_existing,
+                __recursive=True,
+            )
