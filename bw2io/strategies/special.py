8f10420844dfaff53d93d7a3b94ced1b22360524
Michael Weinold <23102087+michaelweinold@users.noreply.github.com>
2023-07-19 07:29:27 +0200

    Merge pull request #193 from Cerebrovinny/documentation-improvements-special
    Update docstrings to NumPy standard `(strategies/parameterization)`

--- bw2io/strategies/special.py
+++ bw2io/strategies/special.py
@@ -1,5 +1,65 @@
 def add_dummy_processes_and_rename_exchanges(db):
-    """Add new processes to link to so-called "dummy" processes in the US LCI database."""
+    """
+    Add new processes to link to so-called "dummy" processes in the US LCI database.
+
+    This function adds new processes to link to dummy processes found in the US LCI
+    database and renames the exchanges accordingly.
+
+    Parameters
+    ----------
+    db : list
+        A list of datasets containing exchanges with dummy processes.
+
+    Returns
+    -------
+    list
+        A modified list of datasets with new processes added and exchanges renamed.
+
+    Examples
+    --------
+    >>> db = [
+            {
+                "database": "uslci",
+                "exchanges": [
+                    {
+                        "name": "dummy_Production",
+                        "input": ("uslci", "dummy_Production"),
+                        "type": "production",
+                        "amount": 1
+                    }
+                ]
+            }
+        ]
+    >>> add_dummy_processes_and_rename_exchanges(db)
+    [
+        {
+            "database": "uslci",
+            "exchanges": [
+                {
+                    "name": "dummy_Production",
+                    "input": ("uslci", "Production"),
+                    "type": "production",
+                    "amount": 1
+                }
+            ]
+        },
+        {
+            "name": "Production",
+            "database": "uslci",
+            "code": "Production",
+            "categories": ("dummy",),
+            "location": "GLO",
+            "type": "process",
+            "exchanges": [
+                {
+                    "input": ("uslci", "Production"),
+                    "type": "production",
+                    "amount": 1
+                }
+            ]
+        }
+    ]
+    """
     new_processes = set()
     for ds in db:
         for exc in ds.get("exchanges"):
