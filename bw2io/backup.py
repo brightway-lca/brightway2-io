bd3892bb84ba05e7e99051f450d9dc27e9493d79
SC-McD <stewart.charles.mcdowall@protonmail.ch>
2023-12-11 19:04:56 +0100

    amend-to-CM-comments

--- bw2io/backup.py
+++ bw2io/backup.py
@@ -1,5 +1,3 @@
-from pathlib import Path
-from typing import Optional
 import codecs
 import datetime
 import json
@@ -7,38 +5,81 @@ import os
 import shutil
 import tarfile
 import tempfile
+from pathlib import Path
+from typing import Optional, Union
 
 from bw2data import projects
 from bw_processing import safe_filename
 
 
-def backup_data_directory():
+def backup_data_directory(
+    timestamp: Optional[bool] = True, dir_backup: Optional[Union[str, Path]] = None
+):
     """
-    Backup data directory to a ``.tar.gz`` (compressed tar archive) in the user's home directory.
-    Restoration is done manually.
+    Backup the Brightway2 data directory to a `.tar.gz` (compressed tar archive) in a specified directory, or in the user's home directory by default.
+
+    The file name is of the form "brightway2-data-backup.{timestamp}.tar.gz", unless `timestamp` is False, in which case the file name is "brightway2-data-backup.tar.gz".
+
+    Parameters
+    ----------
+    timestamp : bool, optional
+        If True, append a timestamp to the backup file name.
+
+    dir_backup : str, Path, optional
+        Directory to backup. If None, use the user's home directory.
+
+    Raises
+    ------
+    FileNotFoundError
+        If the backup directory does not exist.
+    PermissionError
+        If the backup directory is not writable.
 
     Examples
     --------
+    >>> import bw2io
     >>> bw2io.bw2setup()
     >>> bw2io.backup.backup_data_directory()
     Creating backup archive - this could take a few minutes...
     """
-    fp = os.path.join(
-        os.path.expanduser("~"),
-        "brightway2-data-backup.{}.tar.gz".format(
-            datetime.datetime.now().strftime("%d-%B-%Y-%I-%M%p")
-        ),
+
+    dir_backup = Path(dir_backup or Path.home())
+
+    # Check if the backup directory exists and is writable
+    if not dir_backup.exists():
+        raise FileNotFoundError(f"The directory {dir_backup} does not exist.")
+    if not os.access(dir_backup, os.W_OK):
+        raise PermissionError(f"The directory {dir_backup} is not writable.")
+
+    # Construct the backup file name
+    timestamp_str = (
+        f'.{datetime.datetime.now().strftime("%d-%B-%Y-%I-%M%p")}' if timestamp else ""
     )
-    print("Creating backup archive - this could take a few minutes...")
+    backup_filename = f"brightway2-data-backup{timestamp_str}.tar.gz"
+    fp = dir_backup / backup_filename
+
+    # Create the backup archive
+    print("Creating backup archive of data directory - this could take a few minutes...")
     with tarfile.open(fp, "w:gz") as tar:
-        tar.add(projects.dir, arcname=os.path.basename(projects.dir))
+        data_directory = Path(
+            projects.dir
+        )  # Assuming projects.dir is a valid directory path
+        tar.add(data_directory, arcname=data_directory.name)
+
+    print(f"Saved to: {fp}")
+    
+    return fp
 
 
-def backup_project_directory(project: str):
+def backup_project_directory(
+    project: str,
+    timestamp: Optional[bool] = True,
+    dir_backup: Optional[Union[str, Path]] = None,
+):
     """
     Backup project data directory to a ``.tar.gz`` (compressed tar archive) in the user's home directory, or a directory specified by ``dir_backup``.
 
-    File name is of the form ``brightway2-project-{project}-backup.{timestamp}.tar.gz``, unless ``timestamp`` is False, in which case the file name is ``brightway2-project-{project}-backup.tar.gz``.
+    File name is of the form ``brightway2-project-{project}-backup{timestamp}.tar.gz``, unless ``timestamp`` is False, in which case the file name is ``brightway2-project-{project}-backup.tar.gz``.
 
     Parameters
     ----------
@@ -48,7 +89,7 @@ def backup_project_directory(project: str):
     timestamp : bool, optional
         If True, append a timestamp to the backup file name.
 
-    dir_backup : str, optional
+    dir_backup : str, Path, optional
         Directory to backup. If None, use the default (home)).
 
     Returns
@@ -60,6 +101,10 @@ def backup_project_directory(project: str):
     ------
     ValueError
        If the project does not exist.
+    FileNotFoundError
+        If the backup directory does not exist.
+    PermissionError
+        If the backup directory is not writable.
 
     See Also
     --------
@@ -93,20 +138,21 @@ def backup_project_directory(project: str):
         tar.add(dir_path, arcname=safe_filename(project))
 
     print(f"Saved to: {fp}")
-    return project
+
+    return fp
 
 
 def restore_project_directory(
-    fp: str,
+    fp: Union[str, Path],
     project_name: Optional[str] = None,
     overwrite_existing: Optional[bool] = False,
 ):
     """
-    Restore a backed up project data directory from a ``.tar.gz`` (compressed tar archive) specified by ``fp``.
+    Restore a backed up project data directory from a ``.tar.gz`` (compressed tar archive) specified by ``fp``. Choose a custom name, or use the name of the project in the archive. If the project already exists, you must set ``overwrite_existing`` to True.
 
     Parameters
     ----------
-    fp : str
+    fp : str, Path
         File path of the project to restore.
     project_name : str, optional
         Name of new project to create
@@ -114,18 +160,24 @@ def restore_project_directory(
 
     Returns
     -------
-    project_name : str
+    project_name : str, Path
         Name of the project that was restored.
 
     Raises
     ------
+    FileNotFoundError
+        If the file path does not exist.
     ValueError
-       If the project does not exist.
+        If the project name cannot be found in the archive.
+        If the project exists and ``overwrite_existing`` is False.
 
     See Also
     --------
     bw2io.backup.backup_project_directory: To backup a project directory.
     """
+    fp_path = Path(fp)
+    if not fp_path.is_file():
+        raise ValueError(f"Can't find file at path: {fp}")
 
     def get_project_name(fp):
         reader = codecs.getreader("utf-8")
@@ -136,12 +188,13 @@ def restore_project_directory(
                     return json.load(reader(tar.extractfile(member)))["name"]
             raise ValueError("Couldn't find project name file in archive")
 
-    assert os.path.isfile(fp), "Can't find file at path: {}".format(fp)
     print("Restoring project backup archive - this could take a few minutes...")
     project_name = get_project_name(fp) if project_name is None else project_name
 
     if project_name in projects and not overwrite_existing:
-        raise ValueError("Project {} already exists".format(project_name))
+        raise ValueError(
+            f"Project {project_name} already exists, set overwrite_existing=True to overwrite"
+        )
 
     with tempfile.TemporaryDirectory() as td:
         with tarfile.open(fp, "r:gz") as tar:
