60a3b3d9cf40c575fb63b3c9487acaa3f9cee486
Chris Mutel <cmutel@gmail.com>
2024-08-15 08:33:43 +0200

    Add Sentier tests

--- tests/sentier.dev/test_simapro_units.py
+++ tests/sentier.dev/test_simapro_units.py
@@ -49,3 +49,38 @@ def test_integration_SPUC():
     ]
     for obj in expected:
         assert obj in result
+
+
+def test_match_internal_simapro_simapro_with_unit_conversion():
+    given = [
+        {
+            "name": "foo",
+            "location": "bar",
+            "database": "a",
+            "code": "b",
+            "unit": "km",
+            "exchanges": [
+                {"name": "foo", "location": "bar", "unit": "m", "amount": 1000.0}
+            ],
+        }
+    ]
+    expected = [
+        {
+            "name": "foo",
+            "location": "bar",
+            "database": "a",
+            "code": "b",
+            "unit": "km",
+            "exchanges": [
+                {
+                    "input": ("a", "b"),
+                    "name": "foo",
+                    "location": "bar",
+                    "unit": "km",
+                    "loc": 1.0,
+                    "amount": 1.0,
+                }
+            ],
+        }
+    ]
+    assert match_internal_simapro_simapro_with_unit_conversion(given) == expected
